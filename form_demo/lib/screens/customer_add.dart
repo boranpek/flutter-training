7e72d0b055c093571619ab65d1a25de3df96eb8b
boran <boranpekell@hotmail.com>
2019-07-19 13:30:10 +0300

    implement form structure

---
+++ form_demo/lib/screens/customer_add.dart
@@ -0,0 +1,76 @@
+
+
+import 'package:flutter/material.dart';
+
+class CustomerAdd extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => CustomerAddState();
+
+}
+
+class CustomerAddState extends State<CustomerAdd>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(25.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            firstNameField(),
+            lastNameField(),
+            emailField(),
+            passwordField(),
+            submitButton()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget firstNameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: "Name",
+        hintText: "Boran"
+      ),
+    );
+  }
+
+  Widget lastNameField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: "Surname",
+          hintText: "Pekel"
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: "Email",
+          hintText: "...@mail.com"
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: "Password",
+          hintText: "password"
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text("Save"),
+      onPressed: (){
+
+      },
+    );
+  }
+
+
+}
