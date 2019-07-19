04a8fac3590720a7527a211ceed9af9db866f14d
boran <boranpekell@hotmail.com>
2019-07-19 14:55:42 +0300

    detect gestures

--- gestures_demo/lib/main.dart
+++ gestures_demo/lib/main.dart
@@ -18,12 +18,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int tabsCount = 0;
+  int doubleTabsCount = 0;
+  int longPressedCount = 0;
+
   @override
   Widget build(BuildContext context) {
 
@@ -31,20 +34,44 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Gestures Demo"),
       ),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: 40.0,
-            top: 60.0,
-            child: Container(
-              width: 50.0,
-              height: 50.0,
-              decoration: BoxDecoration(
-                color: Colors.deepPurpleAccent
+      body: GestureDetector(
+        onTap: (){
+          setState(() {
+            tabsCount++;
+          });
+        },
+        onDoubleTap: (){
+          setState(() {
+            doubleTabsCount++;
+          });
+        },
+        onLongPress: (){
+          setState(() {
+            longPressedCount++;
+          });
+        },
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 40.0,
+              top: 60.0,
+              child: Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Material(
+        child:
+        Padding(
+          child: Text("Tab : $tabsCount, Double Tab: $doubleTabsCount, Long Press: $longPressedCount"),
+          padding: EdgeInsets.all(10.0),
+        ),
       ),
     );
   }
