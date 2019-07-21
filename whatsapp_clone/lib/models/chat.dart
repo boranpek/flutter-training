8b7fae23385550d4eb4950666fdbdbd91dfbbd26
boran <boranpekell@hotmail.com>
2019-07-21 13:03:09 +0300

    implement chat model

---
+++ whatsapp_clone/lib/models/chat.dart
@@ -0,0 +1,17 @@
+class Chat{
+  String name;
+  String message;
+  String time;
+  String avatarUrl;
+
+  Chat(this.name, this.message, this.time, this.avatarUrl);
+
+  List<Chat> fakeData = [
+    Chat("Boran","Hello","13:30","https://media.licdn.com/dms/image/C4D03AQFydbCDaQjAkw/profile-displayphoto-shrink_200_200/0?e=1569456000&v=beta&t=IYf4wMbOkdI5SaTTKqV7rUrSKx_oyR_Zp84m2Guch-s"),
+    Chat("Boran","How are you?","13:31","https://media.licdn.com/dms/image/C4D03AQFydbCDaQjAkw/profile-displayphoto-shrink_200_200/0?e=1569456000&v=beta&t=IYf4wMbOkdI5SaTTKqV7rUrSKx_oyR_Zp84m2Guch-s"),
+    Chat("Boran","Where are you?","13:33","https://media.licdn.com/dms/image/C4D03AQFydbCDaQjAkw/profile-displayphoto-shrink_200_200/0?e=1569456000&v=beta&t=IYf4wMbOkdI5SaTTKqV7rUrSKx_oyR_Zp84m2Guch-s"),
+
+  ];
+
+
+}
