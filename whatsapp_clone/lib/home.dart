168e8105518ca86f52aec4fd4283989c9a2c3a57
boran <boranpekell@hotmail.com>
2019-07-21 12:53:21 +0300

    implement whatsapp main screen

--- whatsapp_clone/lib/home.dart
+++ whatsapp_clone/lib/home.dart
@@ -6,11 +6,54 @@ class Home extends StatefulWidget{
 
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with SingleTickerProviderStateMixin{
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("HomePage"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsApp"),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "Chat"),
+            Tab(text: "Status"),
+            Tab(text: "Calls")
+          ],
+        ),
+        actions: <Widget>[
+          Icon(Icons.search),
+          Icon(Icons.more_vert)
+
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Text("Camera"),
+          Text("Chat"),
+          Text("Status"),
+          Text("Calls")
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: Icon(
+          Icons.message,
+          color: Colors.white,
+        ),
+        onPressed: (){
+          print("sohbet ekranı");
+        },
+      ),
     );
   }
 }
