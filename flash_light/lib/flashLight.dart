62b31bcdd69892270b21bef0105d8838c395f17f
boran <boranpekell@hotmail.com>
2019-07-23 10:50:20 +0300

    initialize flashLight project

---
+++ flash_light/lib/flashLight.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FlashLight extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => FlashLightState();
+
+}
+
+class FlashLightState extends State<FlashLight> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flash Light"),
+      ),
+      body: ,
+    );
+  }
+}
