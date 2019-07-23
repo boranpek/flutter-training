9ef5dccca93d3ca783b3246ebf13a70a131a4c1b
boran <boranpekell@hotmail.com>
2019-07-23 10:01:04 +0300

    create json file

---
+++ work_with_local_json/lib/localJson.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LocalJson extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => LocalJsonState();
+}
+
+class LocalJsonState  extends State<LocalJson>{
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
